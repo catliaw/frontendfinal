9cf9feca7c0643c4bd12ceef731d8e598d60ae00
catliaw <cat.liaw@gmail.com>
2015-12-09 21:31:24 -0800

    Added images and centered the divs

--- finalproject.js
+++ finalproject.js
@@ -1,3 +1,11 @@
-$(document).get(function(){
-
+$(document).ready(function(){
+	$('.photo').click(function(){
+		$(this).fadeTo('slow', 1, function(){
+			console.log("Faded");
+			})
+		.delay(1500)
+		.fadeTo('slow',0,function(){
+			console.log("Fade Out");
+		});
+	});
 });
